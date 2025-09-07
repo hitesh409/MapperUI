0b33ea311c8ebfc64845dd6813814e96cff5f5e4
Hitesh Prajapati <hiteshprajapati2094@gmail.com>
2025-09-07 17:22:07 +0530

    Mapping and Unmapping function implementation

--- src/app/Components/Mapper/mapper/mapper.component.ts
+++ src/app/Components/Mapper/mapper/mapper.component.ts
@@ -1,11 +1,11 @@
 import { Component, effect, ViewChild, viewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SearchComponent } from "../../../Util/search/search/search.component";
+import { SearchComponent } from '../../../Util/search/search/search.component';
 
-interface MappingModel{
-  id:number,
-  title:string
+interface MappingModel {
+  id: number;
+  title: string;
 }
 
 @Component({
@@ -13,7 +13,7 @@ interface MappingModel{
   standalone: true,
   imports: [CommonModule, FormsModule, SearchComponent],
   templateUrl: './mapper.component.html',
-  styleUrl: './mapper.component.scss'
+  styleUrl: './mapper.component.scss',
 })
 export class MapperComponent {
   unmappedArray: MappingModel[] = [
@@ -29,23 +29,39 @@ export class MapperComponent {
     { id: 10, title: 'item10' },
   ];
   mappedArray: MappingModel[] = [
-    { id: 6, title: 'item6' },
-    { id: 7, title: 'item7' },
-    { id: 8, title: 'item8' },
-    { id: 9, title: 'item9' },
-    { id: 10, title: 'item10' },
+    { id: 16, title: 'item16' },
+    { id: 17, title: 'item17' },
+    { id: 18, title: 'item18' },
+    { id: 19, title: 'item19' },
+    { id: 20, title: 'item20' },
   ];
-  
 
-  @ViewChild('unmappedSearch') unmappedSearch! : SearchComponent;
-  @ViewChild('mappedSearch') mappedSearch! : SearchComponent;
+  @ViewChild('unmappedSearch') unmappedSearch!: SearchComponent;
+  @ViewChild('mappedSearch') mappedSearch!: SearchComponent;
 
-  constructor(){
-    effect(()=>{
+  constructor() {
+    effect(() => {
       debugger;
-      const searchUnmapped = this.unmappedSearch?.searchString() || ''  
-      const searchMapped = this.mappedSearch?.searchString() || ''  
-      console.log(searchMapped,searchUnmapped)
-    })
+      const searchUnmapped = this.unmappedSearch?.searchString() || '';
+      const searchMapped = this.mappedSearch?.searchString() || '';
+      console.log(searchMapped, searchUnmapped);
+    });
+  }
+
+  onMapUnmapItems(item: MappingModel, isMapping:boolean) {
+    if (isMapping) {
+      this.unmappedArray.splice(
+        this.unmappedArray.findIndex((it) => it.id === item.id),
+        1
+      );
+      this.mappedArray.push(item);
+    } else {
+      this.mappedArray.splice(
+        this.mappedArray.findIndex((it) => it.id === item.id),
+        1
+      );
+      this.unmappedArray.push(item);
+      this.unmappedArray.sort((a, b) => a.id - b.id);
+    }
   }
 }
